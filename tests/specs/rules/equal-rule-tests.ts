b80afcf4de955132e69cebf0b051a5b936e5d495
Jason <jason@questmetrics.com>
2016-07-22 17:36:22 +1000

    Added .catch(done) to equality rule Wallaby tests. Other rules still need it adding.

--- tests/specs/rules/equal-rule-tests.ts
+++ tests/specs/rules/equal-rule-tests.ts
@@ -4,12 +4,41 @@ import {EqualValidationRule} from "../../../src/rules/equal-validation-rule";
 describe("Validation Rules", function(){
     describe('Equal Rule', function () {
 
+/*
+        it('should be valid when the comparitor is a function', function (done) {
+            var rule = new EqualValidationRule();
+            rule.validate(10, () => 10).then(function(isValid){
+                expect(isValid).to.be.true;
+                done();
+            }).catch(done);
+        });
+
+        it('should be valid when comparing two model properties', function (done) {
+            var model = { password1: "Identical",password2: "Identical" }
+            var options = model;
+
+            var rule = new EqualValidationRule();
+
+            rule.validate(model.password1, model.password2).then(function(isValid){
+                expect(isValid).to.be.true;
+            }).catch(done);
+
+            model = { password1: "AlsoIdentical",password2: "AlsoIdentical" }
+
+            rule.validate(model.password1, model.password2).then(function(isValid){
+                expect(isValid).to.be.true;
+                done();
+            }).catch(done);
+
+        });
+ */
+
         it('should be valid when numbers equal', function (done) {
             var rule = new EqualValidationRule();
             rule.validate(10, 10).then(function(isValid){
                 expect(isValid).to.be.true;
                 done();
-            });
+            }).catch(done);
         });
 
         it('should be valid when strings equal', function (done) {
@@ -17,7 +46,7 @@ describe("Validation Rules", function(){
             rule.validate("hello", "hello").then(function(isValid){
                 expect(isValid).to.be.true;
                 done();
-            });
+            }).catch(done);
         });
 
         it('should be valid when number weak equality string', function (done) {
@@ -25,7 +54,7 @@ describe("Validation Rules", function(){
             rule.validate(10, { value: "10", weakEquality: true }).then(function(isValid){
                 expect(isValid).to.be.true;
                 done();
-            });
+            }).catch(done);
         });
 
         it('should be valid when dates equal', function (done) {
@@ -33,7 +62,7 @@ describe("Validation Rules", function(){
             rule.validate(new Date(1995, 11, 17), new Date(1995, 11, 17)).then(function(isValid){
                 expect(isValid).to.be.true;
                 done();
-            });
+            }).catch(done);
         });
 
         it('should be valid when provided a null value', function (done) {
@@ -41,7 +70,7 @@ describe("Validation Rules", function(){
             rule.validate(null, {}).then(function(isValid){
                 expect(isValid).to.be.true;
                 done();
-            });
+            }).catch(done);
         });
 
         it('should be invalid when number does not equal same number as string without weak equality', function (done) {
@@ -49,7 +78,7 @@ describe("Validation Rules", function(){
             rule.validate(10, "10").then(function(isValid){
                 expect(isValid).to.be.false;
                 done();
-            });
+            }).catch(done);
         });
 
         it('should be invalid when number does not equal comparison', function (done) {
@@ -57,7 +86,7 @@ describe("Validation Rules", function(){
             rule.validate(10, 20).then(function(isValid){
                 expect(isValid).to.be.false;
                 done();
-            });
+            }).catch(done);
         });
 
     });
